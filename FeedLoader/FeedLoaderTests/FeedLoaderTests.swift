6fc9a97ef8503342cef4609f885602153dd20a20
Shad Mazumder <shadmazumder@gmail.com>
2021-06-09 15:43:55 +0600

    Adding Test target

--- FeedLoader/FeedLoaderTests/FeedLoaderTests.swift
+++ FeedLoader/FeedLoaderTests/FeedLoaderTests.swift
@@ -6,7 +6,6 @@
 //
 
 import XCTest
-@testable import FeedLoader
 
 class FeedLoaderTests: XCTestCase {
 
@@ -25,7 +24,7 @@ class FeedLoaderTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }

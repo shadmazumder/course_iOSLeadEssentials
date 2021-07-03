cf1f03acf921fd95474c29073aa891bb913a7304
Shad Mazumder <shadmazumder@gmail.com>
2021-07-03 18:23:36 +0600

    Initialization returns no data.

--- FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -8,5 +8,31 @@
 import XCTest
 
 class RemoteFeedLoaderTests: XCTestCase {
-
+    struct RemoteFeedLoader{
+        let client: HttpClient
+        
+        init(_ client: HttpClient) {
+            self.client = client
+        }
+    }
+    
+    struct HttpClient {
+        let requestedUrl: String?
+    }
+    
+    func test_init_loadNoData() {
+    let client = HttpClient(requestedUrl: nil)
+    
+    _ = RemoteFeedLoader(client)
+    
+        XCTAssertNil(client.requestedUrl)
+    }
+    
+    func test_load_returnsDataFromUrl() {
+    <#given#>
+    
+    <#when#>
+    
+    <#then#>
+    }
 }

80c52fca0c22612fcd425c2ea7c4cc080d182a8f
Shad Mazumder <shadmazumder@gmail.com>
2021-07-03 18:46:15 +0600

    Initiating HttpClient protocol. And creating HttpClientSpy class for test purpose. Calling the HttpClient get method from RemoteFeedLoader's load method.

--- FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -7,36 +7,48 @@
 
 import XCTest
 
+struct RemoteFeedLoader{
+    let client: HttpClient
+    let url: URL
+    
+    init(client: HttpClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HttpClient {
+    func get(from url: URL)
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
-    struct RemoteFeedLoader{
-        let client: HttpClient
-        
-        init(_ client: HttpClient) {
-            self.client = client
-        }
-        
-        func load() {
-        }
-    }
-    
-    struct HttpClient {
-        let requestedUrl: URL?
-    }
-    
     func test_init_loadsNoData() {
-    let client = HttpClient(requestedUrl: nil)
-    
-    _ = RemoteFeedLoader(client)
-    
+        let client = HttpClientSpy()
+        
+        _ = RemoteFeedLoader(client: client, url: URL(string: "www.mobidevtalk.com")!)
+        
         XCTAssertNil(client.requestedUrl)
     }
     
     func test_load_returnsData() {
-        let client = HttpClient(requestedUrl: URL(string: "www.mobidevtalk.com"))
-        let remoteFeedLoader = RemoteFeedLoader(client)
-    
+        let client = HttpClientSpy()
+        let remoteFeedLoader = RemoteFeedLoader(client: client, url: URL(string: "www.mobidevtalk.com")!)
+        
         remoteFeedLoader.load()
-    
+        
         XCTAssertNotNil(client.requestedUrl)
+    }
+    
+    // MARK: - Utility
+    private class HttpClientSpy: HttpClient {
+        var requestedUrl: URL?
+        
+        func get(from requestedUrl: URL) {
+            self.requestedUrl = requestedUrl
+        }
     }
 }

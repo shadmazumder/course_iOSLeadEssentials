659e5c1260bd6d49e17920853125c7a2db143be5
Shad Mazumder <shadmazumder@gmail.com>
2021-07-04 20:13:46 +0600

    Replacing stubbing with capturing the completion from Spy. Moving to asynchronous behavior by setting the error state after calling the load method. Making sure that, the error completion is happing only once, not multiple times.

--- FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ FeedLoader/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -35,10 +35,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
-    
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load{ capturedError.append($0)}
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complitions[0](clientError)
         
         XCTAssertEqual(capturedError, [.connectivity])
     }
@@ -49,12 +50,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     // MARK: - Utility
     private class HttpClientSpy: HttpClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var complitions = [(Error) -> Void]()
         
         func get(from requestedUrl: URL, completion: @escaping (Error)-> Void) {
-            if let error = error {
-                completion(error)
-            }
+            complitions.append(completion)
             requestedURLs.append(requestedUrl)
         }
     }

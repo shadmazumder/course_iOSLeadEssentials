55dfa36f03d7f2fd0d01a75c16f73db31b6be097
Shad Mazumder <shadmazumder@gmail.com>
2021-07-07 18:37:59 +0600

    Moving FeedItemMapper to a new file.

--- FeedLoader/FeedAPI/RemoteFeedLoader.swift
+++ FeedLoader/FeedAPI/RemoteFeedLoader.swift
@@ -43,29 +43,3 @@ public struct RemoteFeedLoader{
         }
     }
 }
-
-private class FeedItemMapper{
-    private struct Root: Decodable{
-        let items: [Item]
-    }
-    
-    private struct Item: Decodable{
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem{
-            return FeedItem(id: id, description: description, location: location, imageUrl: image)
-        }
-    }
-    
-    static var ok_200: Int { return 200 }
-    
-    static func map(response: HTTPURLResponse, data: Data) throws -> [FeedItem]{
-        guard response.statusCode == ok_200 else{ throw RemoteFeedLoader.Error.invalidData }
-        
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map({ $0.item })
-    }
-}

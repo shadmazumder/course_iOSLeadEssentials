bd3b0c93486542215c9ac01668ee2283ff4e0fd7
Shad Mazumder <shadmazumder@gmail.com>
2021-07-26 18:05:01 +0600

    Replacing Result of FeedApi with RemoteFeedLoader of FeedFeature.

--- FeedLoader/FeedAPI/RemoteFeedLoader.swift
+++ FeedLoader/FeedAPI/RemoteFeedLoader.swift
@@ -14,10 +14,7 @@ public class RemoteFeedLoader{
         case invalidData
     }
     
-    public enum Result: Equatable{
-        case success([FeedItem])
-        case failure(Error)
-    }
+    public typealias Result = LoadFeedResult <Error>
     
     private let url: URL
     private let client: HttpClient

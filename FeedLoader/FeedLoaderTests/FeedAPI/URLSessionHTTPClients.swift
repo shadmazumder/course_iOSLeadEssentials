71d6e918fc57289c1d089016d7f0b104d1376e17
Shad Mazumder <shadmazumder@gmail.com>
2021-08-05 09:34:43 +0600

    Tracking memory leak.

--- FeedLoader/FeedLoaderTests/FeedAPI/URLSessionHTTPClients.swift
+++ FeedLoader/FeedLoaderTests/FeedAPI/URLSessionHTTPClients.swift
@@ -73,7 +73,15 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: - Helper Entity
     func makeSUT() -> URLSessionHTTPClient{
-        return URLSessionHTTPClient()
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeaks(sut)
+        return sut
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line){
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance is not nil. Potential memory leak.", file: file, line: line)
+        }
     }
     
     private class URLProtocolStub: URLProtocol {
